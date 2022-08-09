3aa46b99492ceac5636e58a1f69ded7754e35239
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:11:09 +0000

    add execute_input function

--- shell.c
+++ shell.c
@@ -50,6 +50,54 @@ void run_shell(char **argv, char **env)
 }
 
 /**
+ * execute_input - executes a input string
+ * @argv: the arguments passed form main
+ * @input: the input read from stdin
+ * @cmd_num: the line number of the command
+ * @env_ls: the environment variables
+ *
+ * Return: status of the execution result
+ */
+int execute_input(char **argv, char *input, int cmd_num, EnvList **env_ls)
+{
+	char **c_args = NULL;
+	char *executable = NULL;
+	int status = 0, arr_size = 0;
+	int (*builtin_func)(char *, char **, int, EnvList **);
+
+	c_args = str_to_ary(input, " ", &arr_size);
+	free(input);
+
+	if (c_args == NULL)
+		return (EXIT_FAILURE);
+
+	builtin_func = get_builtin(c_args[0]);
+	if (builtin_func != NULL)
+		status = builtin_func(argv[0], c_args, cmd_num, env_ls);
+	else
+	{
+		if (access(c_args[0], F_OK))
+		{
+			executable = find_in_path(c_args[0], *env_ls);
+			if (executable == NULL)
+				print_error(argv[0], cmd_num, c_args[0], "No such file or directory");
+			else
+			{
+				free(c_args[0]);
+				c_args[0] = _strdup(executable);
+				free(executable);
+				status = run_child_process(c_args, env_ls);
+			}
+		}
+		else
+			status = run_child_process(c_args, env_ls);
+	}
+
+	free_str_ary(c_args);
+	return (status);
+}
+
+/**
  * run_child_process - starts a child process to execute the
  * given argument with the specified environment variables
  * @c_args: command to be executed by the process
