6e43cc81829347dffa9ab557edb293cd9803f9df
callmeoli <olikorma@gmail.com>
2022-08-09 23:53:50 +0300

    count_until_delim

---
+++ _strtok.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * count_until_delim - returns the number of chars until
+ * the next delimiter
+ * @str: the string
+ * @delim: the delimiters
+ *
+ * Return: the number of chars until delim
+ */
+int count_until_delim(char *str, const char *delim)
+{
+	int count = 0, i;
+
+	while (*str != '\0')
+	{
+		for (i = 0; delim[i] != '\0'; i++)
+		{
+			if (delim[i] == *str)
+				return (count);
+		}
+		count++;
+		str++;
+	}
+	return (-1);
+}
