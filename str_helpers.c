0a06f26d761c9d31680a02cc45a1bd363103c0d4
callmeoli <olikorma@gmail.com>
2022-08-10 00:28:09 +0300

    _strcat

--- str_helpers.c
+++ str_helpers.c
@@ -46,4 +46,28 @@ char *_strdup(char *str)
 	dup[i] = '\0';
 	return (dup);
 }
+/**
+ * _strcat - concatenates src string to dest string
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ *
+ * Return: pointer to the dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_i = 0, src_i = 0;
 
+	while (*(dest + dest_i) != '\0')
+		dest_i++;
+
+	while (*(src + src_i) != '\0')
+	{
+		*(dest + dest_i) = *(src + src_i);
+		dest_i++;
+		src_i++;
+	}
+
+	*(dest + dest_i) = '\0';
+
+	return (dest);
+}
