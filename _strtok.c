bb617c25e97ea2c2718a74dbb5e526e4851e1176
callmeoli <olikorma@gmail.com>
2022-08-09 23:56:15 +0300

    _strtok

--- _strtok.c
+++ _strtok.c
@@ -35,3 +35,43 @@ void skip_delim(char **str, char delim)
 	while (*(*str) == delim)
 		(*str)++;
 }
+/**
+ * _strtok - returns the next token in the string delimited
+ * by the delimiters specified
+ * @str: the string
+ * @delims: the delimiters
+ *
+ * Return: the next token or NULL
+ */
+char *_strtok(char *str, const char *delims)
+{
+	static char *ch_ptr;
+	char *token;
+	int count = 0, i;
+
+	if (str != NULL)
+		ch_ptr = str;
+
+	if (ch_ptr == NULL)
+		return (NULL);
+
+	for (i = 0; delims[i] != '\0'; i++)
+		skip_delim(&ch_ptr, delims[i]);
+
+	if (*ch_ptr == '\0')
+		return (NULL);
+
+	count = count_until_delim(ch_ptr, delims);
+	if (count == -1)
+		count = strlen(ch_ptr);
+
+	token = (char *)malloc(sizeof(char) * (count + 1));
+	for (i = 0; i < count; i++)
+	{
+		token[i] = *ch_ptr;
+		ch_ptr++;
+	}
+	token[count] = '\0';
+
+	return (token);
+}
