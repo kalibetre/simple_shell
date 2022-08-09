20695ef4249df4505d19cee54d51c30354120edf
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:16:52 +0000

    add print_env function

--- builtins.c
+++ builtins.c
@@ -25,3 +25,23 @@ int exit_shell(char *sh_name, char **c_args, int cmd_num, EnvList **env_ls)
 	exit(exit_status);
 }
 
+/**
+ * print_env - prints the current environment variables
+ * @sh_name: the name of the current shell
+ * @c_args: command to be executed by the builtin
+ * @cmd_num: the line number of the command
+ * @env_ls: the environment variables
+ *
+ * Return: status of the function execution
+ */
+int print_env(char *sh_name, char **c_args, int cmd_num, EnvList **env_ls)
+{
+	(void)sh_name;
+	(void)cmd_num;
+	(void)c_args;
+
+	print_env_list(*env_ls);
+
+	return (0);
+}
+
