c3e69635c8ec65134d4f6d5a0bdba06ff231ab5c
callmeoli <olikorma@gmail.com>
2022-08-09 23:59:22 +0300

    str_ary_dup

---
+++ arry_helpers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * str_ary_dup - duplicates an array of strings
+ * @ary: the array to be duplicated
+ * @start_index: the start index
+ * @count: the number items to copy
+ *
+ * Return: returns the duplicated array or NULL
+ */
+char **str_ary_dup(char **ary, size_t start_index, size_t count)
+{
+	char **dup;
+	size_t i;
+
+	dup = malloc(sizeof(char *) * count);
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		dup[i] = _strdup(ary[i + start_index]);
+
+	return (dup);
+}
