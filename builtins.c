d2fe0580950c3b66e91a15b0b003a58465fe5fda
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:16:28 +0000

    add exit_shell function

---
+++ builtins.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * exit_shell - exit's the shell
+ * @sh_name: the name of the current shell
+ * @c_args: command to be executed by the builtin
+ * @cmd_num: the line number of the command
+ * @env_ls: the environment variables
+ *
+ * Return: status of the function execution
+ */
+int exit_shell(char *sh_name, char **c_args, int cmd_num, EnvList **env_ls)
+{
+	int exit_status = EXIT_SUCCESS;
+
+	(void)sh_name;
+	(void)cmd_num;
+	(void)env_ls;
+
+	if (c_args[1] != NULL)
+		exit_status = _atoi(c_args[1]);
+
+	free_str_ary(c_args);
+	free_env_list(*env_ls);
+	exit(exit_status);
+}
+
