9b7232a0f2019b5e189b213c545947f6e8b60ccd
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:10:49 +0000

    add run_shell function

--- shell.c
+++ shell.c
@@ -1,4 +1,54 @@
 #include "main.h"
+
+/**
+ * run_shell - runs a simple shell until the user exits
+ * @argv: the program's argument variables
+ * @env: the environment variables of the shell
+ *
+ * Return: Nothing
+ */
+void run_shell(char **argv, char **env)
+{
+	int status = 0, cmd_num = 0;
+	char *line = NULL;
+	ssize_t line_len = 0;
+	EnvList *env_ls;
+
+	env_ls = build_env_list(argv[0], env);
+
+	do {
+		cmd_num++;
+		if (isatty(STDIN_FILENO))
+		{
+			cmd_num = 0;
+			_print("#cisfun$ ");
+			fflush(stdout);
+		}
+
+		line_len = _getline(&line);
+		if (line_len <= 0)
+		{
+			if (isatty(STDIN_FILENO))
+				_putchar('\n');
+			free_env_list(env_ls);
+			exit(EXIT_SUCCESS);
+		}
+
+		if (line[0] == '\0')
+		{
+			free(line);
+			line = NULL;
+			continue;
+		}
+
+		status = execute_input(argv, line, cmd_num, &env_ls);
+		line = NULL;
+	} while (status == 0);
+
+	free_env_list(env_ls);
+	exit(status);
+}
+
 /**
  * run_child_process - starts a child process to execute the
  * given argument with the specified environment variables
