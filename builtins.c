c7888384403b8d70ebe6c5ccad8d09a83c864b47
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:17:21 +0000

    add set_env function

--- builtins.c
+++ builtins.c
@@ -45,3 +45,23 @@ int print_env(char *sh_name, char **c_args, int cmd_num, EnvList **env_ls)
 	return (0);
 }
 
+/**
+ * set_env - sets an environment variable
+ * @sh_name: the name of the current shell
+ * @c_args: command to be executed by the builtin
+ * @cmd_num: the line number of the command
+ * @env_ls: the environment variables
+ *
+ * Return: status of the function execution
+ */
+int set_env(char *sh_name, char **c_args, int cmd_num, EnvList **env_ls)
+{
+	if (c_args[1] == NULL)
+		print_error(sh_name, cmd_num, c_args[0], "Key and Value Required");
+	else if (c_args[2] == NULL)
+		print_error(sh_name, cmd_num, c_args[0], "Value required");
+	else
+		_set_env(sh_name, c_args, cmd_num, env_ls);
+	return (0);
+}
+
