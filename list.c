50d75d30b5640e1f4ee6b667a75952839c00b287
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:08:30 +0000

    add add_to_str_list function

--- list.c
+++ list.c
@@ -17,3 +17,39 @@ void free_str_list(list_s *list)
 		free(list);
 	}
 }
+
+/**
+ * add_to_list - adds an element to a list
+ * @head: pointer to the head of a list
+ * @str: data to be added
+ *
+ * Return: a pointer to the list or NULL
+ */
+list_s *add_to_str_list(list_s **head, const char *str)
+{
+	list_s *node;
+
+	if (str == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(list_s));
+	if (node == NULL)
+		return (NULL);
+
+	node->str = _strdup((char *)str);
+	if (node->str == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	if (head == NULL)
+	{
+		node->next = NULL;
+		return (node);
+	}
+
+	node->next = *head;
+	*head = node;
+	return (node);
+}
