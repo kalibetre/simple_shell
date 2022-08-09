b79c5ebe514166b05fd2e7f964a4894c0824993c
callmeoli <olikorma@gmail.com>
2022-08-10 00:18:40 +0300

    _putchar_er

--- print_helpers.c
+++ print_helpers.c
@@ -11,3 +11,14 @@ int _putchar(char c)
 {
 	return (write(STDOUT_FILENO, &c, 1));
 }
+/**
+ * _putchar_er - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar_er(char c)
+{
+	return (write(STDERR_FILENO, &c, 1));
+}
