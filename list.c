a4974c7692b217e646033776df38d23cea49a0c1
kalbetre-alx <kalbetre@icloud.com>
2022-08-09 20:25:19 +0000

    add str_to_str_list function

--- list.c
+++ list.c
@@ -53,3 +53,26 @@ list_s *add_to_str_list(list_s **head, const char *str)
 	*head = node;
 	return (node);
 }
+
+/**
+ * str_to_list - splits a string into an list of strings
+ * @str: the string to be converted to list
+ * @delim: delimeter to use
+ *
+ * Return: a NULL terminated array of strings
+ */
+list_s *str_to_str_list(char *str, const char *delim)
+{
+	list_s *str_list = NULL;
+	char *token;
+
+	token = _strtok(str, delim);
+	while (token != NULL)
+	{
+		str_list = add_to_str_list(&str_list, token);
+		free(token);
+		token = _strtok(NULL, delim);
+	}
+
+	return (str_list);
+}
